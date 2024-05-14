d15eddb619c67e436f040f698b71797f8ddb040f
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-05-14 15:48:52 +0700

    function exercise 2

---
+++ script.js
@@ -0,0 +1,21 @@
+//Exercise 2.1
+const checkEven = function(number){
+    if (number %2 === 0){
+        console.log("event number");
+    }
+    else {
+        console.log ("odd number");
+    }
+};
+checkEven(4)
+
+//Exercise 2.2
+const checkEvenNumber = (number) =>{
+    if (number %2 === 0){
+        console.log("event number");
+    }
+    else {
+        console.log ("odd number");
+    }
+};
+checkEvenNumber(3)
